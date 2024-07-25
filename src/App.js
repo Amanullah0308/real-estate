dc4f37b62ba3752a7607364c71727881118607a6
Amanullah <amanullah128@gmail.com>
2024-07-25 14:30:11 +0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profiler from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-orange-400">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profiler />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>This is sign In component</div>;
+};
+
+export default SignIn;
